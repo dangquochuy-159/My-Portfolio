f5b332e2e4e703d5a67242504dc56e68ba5941d1
dangquochuy <quochuy22122002@gmail.com>
2024-03-26 12:50:58 +0700

    feat (header): add component header

---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { DataHeader } from '@/data/data_portfolio';
+import Image from 'next/image';
+import Link from 'next/link';
+
+function Header() {
+    const data_header = DataHeader;
+
+    return (
+        <header className="h-[100px] w-full bg-transparent text-white fixed top-0 left-0">
+            <div className="container h-full m-auto">
+                <nav className="h-full">
+                    <ul className="h-full flex justify-end items-center gap-10">
+                        {data_header.nav?.map((data) => (
+                            <li key={data.href} className="nav--header">
+                                <Link href={data.href} className="p-2 rounded-md">
+                                    {data.name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
