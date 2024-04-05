a3639a3db9a2e7c7c6d9d05691c60102cae2d038
dangquochuy-student <2051120123@ut.edu.vn>
2024-04-05 17:12:39 +0700

    fix (page): update interface page about me

--- my-app/src/app/(MainLayout)/about-me/page.tsx
+++ my-app/src/app/(MainLayout)/about-me/page.tsx
@@ -1,7 +1,7 @@
 'use client';
-import BoxInfo from '@/components/BoxInfo/BoxInfo';
+import ImageTag from '@/components/ImageTag/ImageTag';
 import TitlePage from '@/components/TitlePage/TitlePage';
-import { Data_Education, Data_Personal_Info, Data_Skills } from '@/data/data_portfolio';
+import { DataIntroduce, Data_Education, Data_Personal_Info, Data_Skills } from '@/data/data_portfolio';
 
 function AboutMe() {
     const data_personal_info = Data_Personal_Info;
@@ -15,10 +15,98 @@ function AboutMe() {
                 des="I'm a fullstack web developer, proficient in both front-end and back-end programming. I specialize in
                     creating websites that are modern and optimized for user experience."
             />
-            <section className="md:mt-20 grid grid-cols-1 xl:grid-cols-3 gap-10">
-                <BoxInfo title_box="Personal Information" data={data_personal_info} />
-                <BoxInfo title_box="Education - Certificate" data={data_education} />
-                <BoxInfo title_box="Programming skills" data={data_skills} />
+            <section className="w-full space-y-24 mt-24 pb-24">
+                {/* Personal */}
+                <div className="grid grid-cols-5 w-full">
+                    <div className="col-span-5 xl:col-span-2 w-full h-full">
+                        <ImageTag
+                            src={data_personal_info.img}
+                            alt="logo"
+                            className=" w-3/5 xl:w-2/3 h-full rounded-lg overflow-hidden mx-auto"
+                            classImage="!relative object-contain object-to  object-top"
+                        />
+                    </div>
+                    <div className="col-span-5 xl:col-span-3 w-full h-auto">
+                        <ul className="flex flex-col gap-14 text-[var(--color-text)] mt-3  ">
+                            <h2 className="font-bold text-2xl text-[var(--color-primary)] text-center xl:text-left">
+                                Personal Information
+                            </h2>
+                            {data_personal_info.info.map((item) => (
+                                <li
+                                    key={item.field_text}
+                                    className="flex gap-5 items-center justify-center xl:justify-start text-center"
+                                >
+                                    <span className="hidden xl:block">{item.icon} </span>
+                                    <span>{item.field_text}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+
+                {/* Education */}
+                <div className="grid grid-cols-5 w-full">
+                    <div className="block xl:hidden col-span-5 xl:col-span-2 w-full h-full">
+                        <ImageTag
+                            src={data_education.img}
+                            alt="logo"
+                            className=" w-2/3 h-full rounded-lg overflow-hidden  mx-auto"
+                            classImage="!relative object-contain object-top"
+                        />
+                    </div>
+                    <div className="col-span-5 xl:col-span-3 w-full h-auto">
+                        <ul className="flex flex-col gap-14 text-[var(--color-text)] mt-3 ">
+                            <h2 className="font-bold text-2xl text-[var(--color-primary)] text-center xl:text-right">
+                                Education - Certificate
+                            </h2>
+                            {data_education.info.map((item) => (
+                                <li
+                                    key={item.field_text}
+                                    className="flex xl:justify-end gap-5 items-center justify-center text-center"
+                                >
+                                    <span>{item.field_text}</span>
+                                    <span className="hidden xl:block">{item.icon} </span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="hidden xl:block col-span-5 xl:col-span-2 w-full h-full">
+                        <ImageTag
+                            src={data_education.img}
+                            alt="logo"
+                            className=" w-2/3 h-full rounded-lg overflow-hidden  mx-auto"
+                            classImage="!relative object-contain object-top"
+                        />
+                    </div>
+                </div>
+
+                {/* Skills Programming */}
+                <div className="grid grid-cols-5 w-full space-x-0 xl:space-x-14 ">
+                    <div className="col-span-5 xl:col-span-2 w-full h-full">
+                        <ImageTag
+                            src={data_skills.img}
+                            alt="logo"
+                            className="w-2/3 xl:w-full h-full rounded-lg overflow-hidden  mx-auto"
+                            classImage="!relative object-contain object-top"
+                        />
+                    </div>
+                    <div className="col-span-5 xl:col-span-3 w-full h-auto">
+                        <ul className="flex flex-col gap-14 text-[var(--color-text)] mt-3">
+                            <h2 className="font-bold text-2xl text-[var(--color-primary)] text-center xl:text-left">
+                                Programming skills
+                            </h2>
+                            {data_skills.info.map((item) => (
+                                <li
+                                    key={item.field_text}
+                                    className="flex gap-5 items-center justify-center xl:justify-start text-center"
+                                >
+                                    <span className="hidden xl:block">{item.icon} </span>
+                                    <span>{item.field_text}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </section>
         </div>
     );
