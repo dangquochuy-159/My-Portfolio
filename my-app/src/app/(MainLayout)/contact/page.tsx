135ef11c2aee86e148fae89752632625d1ea0456
dangquochuy-student <2051120123@ut.edu.vn>
2024-04-10 01:24:06 +0700

    feat(send mail): add feature send mail to mail from form contact

--- my-app/src/app/(MainLayout)/contact/page.tsx
+++ my-app/src/app/(MainLayout)/contact/page.tsx
@@ -1,8 +1,45 @@
+'use client';
 import { Icon_Facebook, Icon_Location, Icon_Mail, Icon_Phone } from '@/components/Icon/Icon';
 import ImageTag from '@/components/ImageTag/ImageTag';
 import TitlePage from '@/components/TitlePage/TitlePage';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { useRef } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Contact() {
+    const refToName = useRef<HTMLInputElement>(null);
+    const refToMail = useRef<HTMLInputElement>(null);
+    const refPhone = useRef<HTMLInputElement>(null);
+    const refMessage = useRef<HTMLTextAreaElement>(null);
+
+    const service_id: string = process.env.NEXT_PUBLIC_SERVICE_ID ?? '';
+    const template_id: string = process.env.NEXT_PUBLIC_TEMPLATE_ID ?? '';
+    const public_key: string = process.env.NEXT_PUBLIC_PUBLIC_KEY ?? '';
+
+    const SendEmail = async () => {
+        const templateParams = {
+            from_name: process.env.NEXT_PUBLIC_FROM_MAIL,
+            mail_to: refToMail.current?.value,
+            to_name: refToName.current?.value,
+            message: refMessage.current?.value,
+        };
+
+        try {
+            await emailjs.send(service_id, template_id, templateParams, {
+                publicKey: public_key,
+            });
+
+            toast('Thank you for contacting me, your information has been sent successfully');
+        } catch (err) {
+            if (err instanceof EmailJSResponseStatus) {
+                console.log('EMAILJS FAILED...', err);
+                return;
+            }
+
+            console.log('ERROR', err);
+        }
+    };
     return (
         <div className="py-[var(--height-header)] md:py-0">
             <TitlePage
@@ -43,6 +80,22 @@ function Contact() {
                     </a>
                 </div>
             </div>
+            <div className="pb-48">
+                <div className="flex flex-col gap-y-5 w-1/2">
+                    <label className="text-white">Your Name</label>
+                    <input ref={refToName} className="border-2 border-solid border-black text-black bg-white" />
+                    <label className="text-white">Your Phone</label>
+                    <input ref={refPhone} className="border-2 border-solid border-black text-black bg-white" />
+                    <label className="text-white">Your Mail</label>
+                    <input ref={refToMail} className="border-2 border-solid border-black text-black bg-white" />
+                    <label className="text-white">Your Message</label>
+                    <textarea ref={refMessage} className="border-2 border-solid border-black text-black bg-white" />
+                    <button className="bg-red-500 text-white" onClick={SendEmail}>
+                        Submit
+                    </button>
+                </div>
+            </div>
+            <ToastContainer />
         </div>
     );
 }
